8afe8b05e56dc291d76aa1fcd2014ca59fded14a
José Martínez <josegabrielmartinez397@gmail.com>
2026-09-19 20:40:39 -0300

    feat: static export y deploy a GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/Terapias",
+  assetPrefix: "/Terapias",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
